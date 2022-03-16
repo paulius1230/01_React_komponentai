8782c911d2d523c030679153729cb824a4dc79f2
paulius1230 <96494731+paulius1230@users.noreply.github.com>
2022-03-16 02:08:05 -0700

    Add files via upload

---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+import Album from './Album';
+
+function Main(){
+    return (
+<main>
+<section className="py-5 text-center container">
+  <div className="row py-lg-5">
+    <div className="col-lg-6 col-md-8 mx-auto">
+      <h1 className="fw-light">Album example</h1>
+      <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+      <p>
+        <a href="#" className="btn btn-primary my-2">Main call to action</a>
+        <a href="#" className="btn btn-secondary my-2 ms-1">Secondary action</a>
+      </p>
+    </div>
+  </div>
+</section>
+
+<div className="album py-5 bg-light">
+  <div className="container">
+    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+        <Album tekstas="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."/>
+        <Album tekstas="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+        <Album tekstas="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+        <Album tekstas="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+        <Album tekstas="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+        <Album tekstas="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+        <Album tekstas="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+        <Album tekstas="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+        <Album tekstas="This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer." />
+      </div>
+    </div>
+</div>
+</main>
+      );
+    }
+    
+export default Main;
